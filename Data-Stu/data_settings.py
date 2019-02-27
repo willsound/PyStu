7f37d4e33a5eaadb94196a6393db075a06c1d844
huixiang sun <willsound@126.com>
2019-02-27 14:02:28 +0800

    Merge branch 'master' of https://github.com/willsound/PyStu
    # Conflicts:
    #	.idea/PyStu.iml

--- Data-Stu/data_settings.py
+++ Data-Stu/data_settings.py
@@ -1,9 +1,10 @@
 from matplotlib.font_manager import FontProperties
 
 
-class Settings():
+class DataSettings():
     def __init__(self):
         self.data_file_path = r'Data\data.txt'
+        self.data_cluster_file_path = r'Data\cluster.txt'
         self.font_file_path = r'Resources\msyh.ttc'
         self.font_title = FontProperties(fname=self.font_file_path, size=14)
         self.font_xlable = FontProperties(fname=self.font_file_path, size=10)
